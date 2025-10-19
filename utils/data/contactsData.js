4f4857951781c6ef6e7e304bb1ada30c0480f2b5
CodingWorld <codingworldpremium@gmail.com>
2025-10-19 17:07:33 +0530

    Premium Developer Portfolio

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'codingworldpremium@gmail.com',
+    phone: '+91 9876543210',
+    address: 'Mumbai, India ',
+    github: 'https://github.com/cwnotfound',
+    facebook: 'https://www.facebook.com/cwnotfound/',
+    linkedIn: 'https://www.linkedin.com/in/cwnotfound/',
+    twitter: 'https://twitter.com/cwnotfound',
+    stackOverflow: 'https://stackoverflow.com/cwnotfound',
+    devUsername: "cwnotfound"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Coding World",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  description: "My name is Coding World. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'codingworldpremium@gmail.com',
+  phone: '+91 9876543210',
+  address: 'Mumbai, India ',
+  github: 'https://github.com/cwnotfound',
+  facebook: 'https://www.facebook.com/cwnotfound/',
+  linkedIn: 'https://www.linkedin.com/in/cwnotfound/',
+  twitter: 'https://twitter.com/cwnotfound',
+  stackOverflow: 'https://stackoverflow.com/cwnotfound',
+  leetcode: "https://leetcode.com/cwnotfound/",
+  devUsername: "cwnotfound",
+  resume: "https://drive.google.com/file/d/cwnotfound"
+}
